12a069e870b7c8cb572341995779eac8e34d1ab4
José Augusto <Joseaugusto7186@protonmail.com>
2023-04-14 11:13:20 -0300

    aula 3 pt3

--- src/auth/helpers/messages.helper.ts
+++ src/auth/helpers/messages.helper.ts
@@ -2,5 +2,5 @@ export const MessagesHelper = {
     AUTH_LOGIN_NOT_FOUND: 'Login informado não é valido',
     AUTH_PASSWORD_NOT_FOUND: 'Senha informada não é valida',
     AUTH_PASSWORD_OR_LOGIN_NOT_FOUND: 'E-mail e/ou senha não encontrados',
-
+   
 }
